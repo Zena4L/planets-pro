35b39af7f6701e2af42bc17707031c09a077c7f2
Clement Bogyah <Zena@Clements-MacBook-Pro.local>
2022-08-26 12:07:23 +0000

    change

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const fs = require("fs");
 const { parse } = require("csv-parse");
+
+const results = [];
+fs.createReadStream("kepler-data.csv")
+  .on("data", (data) => {
+    results.push(data);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(results);
+  });
